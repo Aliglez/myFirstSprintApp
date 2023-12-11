2d9fa1eda1903a442c21185fe856243ad296e5d6
Alicia González Castro <aliglezcastro@gmail.com>
2023-12-11 18:50:35 +0100

    first commit

---
+++ src/main/java/ali/glez/myFirstSprintApp/MyFirstSprintAppApplication.java
@@ -0,0 +1,13 @@
+package ali.glez.myFirstSprintApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstSprintAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstSprintAppApplication.class, args);
+	}
+
+}
